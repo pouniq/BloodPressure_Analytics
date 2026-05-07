5d97fe56b87105fa845b8b688ad25eb61e710803
pouniq <abdollahpora@gmail.com>
2026-05-07 04:31:24 +0330

    done with the EDA lets do preprocessing and POLARS IS BLAZING FAST :)

--- Utils/Melted.py
+++ Utils/Melted.py
@@ -19,74 +19,176 @@ def df_melt(dataset):
 
 
 
-class Make_dataframe:
+# class Make_dataframe:
     
-    # ''''
-    # In this class If you put any excel data,
-    # You get the result of separated Right 
-    # and Left Arms. with the blood pressure measurements
+#     # ''''
+#     # In this class If you put any excel data,
+#     # You get the result of separated Right 
+#     # and Left Arms. with the blood pressure measurements
 
-    # ''''
-    import pandas as pd
+#     # ''''
+#     import pandas as pd
     
-    df = pd.read_excel('../Data/BP.xlsx')
     
-    def __init__(self, df):
-        df = df
+#     def __init__(self, df):
+#         self.df = df
         
         
-    def Decompose(self):
-        self.df['day'] = self.df['day'].ffill()
-        self.df['hand'] = self.df['hand'].ffill()
-        self.df['date'] = self.df['date'].ffill()
+#     def Decompose(self):
+#         self.df['day'] = self.df['day'].ffill()
+#         self.df['hand'] = self.df['hand'].ffill()
+#         self.df['date'] = self.df['date'].ffill()
         
         
-    def Filter_hand(self):
+#     def Filter_hand(self):
     
-        hands = ['R','L']
-        for hand in hands:
-            if hand == 'R':
-                df_R = self.df[self.df['hand'] == 'R']
-                df_R = df_R.drop('hand' , axis=1)
-                df_R_s = df_R[df_R['type'] == 'Systolic'] 
-                df_R_d =  df_R[df_R['type'] == 'Diastolic'] 
-                df_R_p =  df_R[df_R['type'] == 'Pulse']
+#         hands = ['R','L']
+#         for hand in hands:
+#             if hand == 'R':
+#                 self.df_R = self.df[self.df['hand'] == 'R']
+#                 self.df_R = df_R.drop('hand' , axis=1)
+#                 self.df_R_s = df_R[df_R['type'] == 'Systolic'] 
+#                 self.df_R_d =  df_R[df_R['type'] == 'Diastolic'] 
+#                 self.df_R_p =  df_R[df_R['type'] == 'Pulse']
             
-            else:
-                df_L = self.df[self.df['hand'] == 'L']
-                df_L = df_L.drop('hand' , axis=1)
-                df_L_s = df_L[df_L['type'] == 'Systolic'] 
-                df_L_d =  df_L[df_L['type'] == 'Diastolic'] 
-                df_L_p =  df_L[df_L['type'] == 'Pulse']
+#             else:
+#                 self.df_L = self.df[self.df['hand'] == 'L']
+#                 self.df_L = df_L.drop('hand' , axis=1)
+#                 self.df_L_s = df_L[df_L['type'] == 'Systolic'] 
+#                 self.df_L_d =  df_L[df_L['type'] == 'Diastolic'] 
+#                 self.df_L_p =  df_L[df_L['type'] == 'Pulse']
                 
                 
-    def df_melt(self,dataset):
+#     def df_melt(self,dataset):
             
-        df_long = dataset.melt(
-            id_vars=['day', 'date', 'type'], 
-            value_vars=['morning', 'noon', 'night'],
-            var_name='time_of_day', 
-            value_name='measurement'
-        )
+#         df_long = dataset.melt(
+#             id_vars=['day', 'date', 'type'], 
+#             value_vars=['morning', 'noon', 'night'],
+#             var_name='time_of_day', 
+#             value_name='measurement'
+#         )
         
-        time_order = ['morning', 'noon', 'night']
-        df_long['time_of_day'] = pd.Categorical(df_long['time_of_day'], categories=time_order, ordered=True)
+#         time_order = ['morning', 'noon', 'night']
+#         df_long['time_of_day'] = pd.Categorical(df_long['time_of_day'], categories=time_order, ordered=True)
 
-        df_long = df_long.sort_values(by=['date', 'time_of_day']).reset_index(drop=True)
-        print(df_long)
+#         df_long = df_long.sort_values(by=['date', 'time_of_day']).reset_index(drop=True)
+#         print(df_long)
 
-        return pd.DataFrame(df_long)   
+#         return pd.DataFrame(df_long)   
     
     
-    def rename(self):
-        pass
+#     def rename(self):
+#         self.df_L_dt.rename(columns={'measurement': 'Diastolic'}, inplace)
+#         self.df_L_st.rename(columns={'measurement': 'Systolic'})
+#         sefl.df_L_pt.rename(columns={'measurement': 'Pulse'})
+
+
+
+#     def merge(self):
+#         dataframes = [df_L_st, df_L_pt]
+
+#         df_L = pd.merge(df_L_dt, df_L_st, how='right', on=['date', 'time_of_day','day'])
+#         df_L = pd.merge(df_L, df_L_pt, how='inner', on=['date', 'time_of_day','day'])
+#         df_L.rename(columns= {'measurement': 'Pulse', 'measurement_x':'Diastolic', 'measurement_y':'Systolic'}, inplace=True)
+#         df_L.drop(columns=['type_x', 'type_y', 'type'], inplace=True)
+#         df_L = df_L.sort_values(by='date', ascending=False)
+#         df_L
+ 
+#     def Change_format(self):
+    
+#         df_L['day'] = pd.to_numeric(df_L['day'])
+#         df_L['date'] = pd.to_datetime(df_L['date'], format='%Y/%m/%d')
+
         
-    
-    def Change_format(self):
-    
-        df_L['day'] = pd.to_numeric(df_L['day'])
-        df_L['date'] = pd.to_datetime(df_L['date'], format='%Y/%m/%d')
-
+#         df_R['day'] = pd.to_numeric(df_R['day'])
+#         df_R['date'] = pd.to_datetime(df_R['date'], format='%Y/%m/%d')
         
-        df_R['day'] = pd.to_numeric(df_R['day'])
-        df_R['date'] = pd.to_datetime(df_R['date'], format='%Y/%m/%d')
+        
+        
+        
+        
+        
+        
+        
+    # GAP gpt    
+# import pandas as pd
+
+# class Make_dataframe:
+#     """
+#     This class takes BP excel data and separates Right and Left arm
+#     blood pressure measurements.
+#     """
+
+#     def __init__(self, df):
+#         self.df = df.copy()
+#         self.df_R = None
+#         self.df_L = None
+
+#     def decompose(self):
+#         self.df['day'] = self.df['day'].ffill()
+#         self.df['hand'] = self.df['hand'].ffill()
+#         self.df['date'] = self.df['date'].ffill()
+#         return self.df
+
+#     def filter_hand(self):
+#         self.df_R = self.df[self.df['hand'] == 'R'].drop(columns=['hand']).copy()
+#         self.df_L = self.df[self.df['hand'] == 'L'].drop(columns=['hand']).copy()
+#         return self.df_R, self.df_L
+
+#     def df_melt(self, dataset):
+#         df_long = dataset.melt(
+#             id_vars=['day', 'date', 'type'],
+#             value_vars=['morning', 'noon', 'night'],
+#             var_name='time_of_day',
+#             value_name='measurement'
+#         )
+
+#         time_order = ['morning', 'noon', 'night']
+#         df_long['time_of_day'] = pd.Categorical(
+#             df_long['time_of_day'],
+#             categories=time_order,
+#             ordered=True
+#         )
+
+#         df_long = df_long.sort_values(by=['date', 'time_of_day']).reset_index(drop=True)
+#         return df_long
+
+#     def prepare_side(self, side_df):
+#         """
+#         Converts one side dataframe into a combined table with:
+#         day, date, time_of_day, Diastolic, Systolic, Pulse
+#         """
+#         long_df = self.df_melt(side_df)
+
+#         # Split by type
+#         df_d = long_df[long_df['type'] == 'Diastolic'].rename(columns={'measurement': 'Diastolic'})
+#         df_s = long_df[long_df['type'] == 'Systolic'].rename(columns={'measurement': 'Systolic'})
+#         df_p = long_df[long_df['type'] == 'Pulse'].rename(columns={'measurement': 'Pulse'})
+
+#         # Keep only needed columns
+#         base_cols = ['day', 'date', 'time_of_day']
+
+#         df_final = df_d[base_cols + ['Diastolic']].merge(
+#             df_s[base_cols + ['Systolic']],
+#             on=base_cols,
+#             how='outer'
+#         ).merge(
+#             df_p[base_cols + ['Pulse']],
+#             on=base_cols,
+#             how='outer'
+#         )
+
+#         df_final['day'] = pd.to_numeric(df_final['day'], errors='coerce')
+#         df_final['date'] = pd.to_datetime(df_final['date'], errors='coerce')
+
+#         df_final = df_final.sort_values(by=['date', 'time_of_day']).reset_index(drop=True)
+#         return df_final
+
+#     def process(self):
+#         self.decompose()
+#         self.filter_hand()
+
+#         result_R = self.prepare_side(self.df_R)
+#         result_L = self.prepare_side(self.df_L)
+
+#         return result_R, result_L
--- Utils/StatMelt.py
+++ Utils/StatMelt.py
@@ -4,3 +4,16 @@ def Average_Melt(meltedList, name):
         print('-'*24 , f'Average value for {n}', ''*25)
         print(d.groupby('time_of_day', observed=False )['measurement'].mean())
         print('-'*50)
+        
+        
+        
+
+
+def iqr_bounds(series, factor=1.5):
+        q1 = series.quantile(0.25)
+        q2 = series.quantile(0.75)
+        iqr = q2 - q1
+        lower_bound = q1 - factor * iqr
+        upper_bound = q2 + factor * iqr
+
+        return lower_bound, upper_bound, iqr
